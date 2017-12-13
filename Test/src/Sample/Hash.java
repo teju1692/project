e1ddd92c54b5443c47767affe4a031424d68dc31
Adithya <Adithya@yaganti>
2017-12-13 16:18:45 -0600

    First commit

---
+++ Test/src/Sample/Hash.java
@@ -0,0 +1,35 @@
+package Sample;
+
+import java.io.Serializable;
+import java.util.*;
+
+public class Hash extends AbstractSet implements Set,Cloneable,Serializable{
+	@Override
+	public Iterator iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	public static void main(String args[])
+	{
+		TreeSet<String> h=new TreeSet<String>();
+		h.add("white");
+		h.add("green");
+        h.add("orange");
+		h.add("white");
+		h.add("yellow");
+		Iterator itr=h.iterator();
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
+	}
+
+	
+
+}
---
+++ Test/src/Sample/Linkedlist.java
@@ -0,0 +1,173 @@
+package Sample;
+import java.util.AbstractSequentialList;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Linkedlist extends AbstractSequentialList implements List,Deque{
+	public static void main(String args[])
+	{
+		List<String> l=new LinkedList<String>();
+		l.add("tejaswi");
+		l.add("nadipineni");
+		l.add("swathi");
+		l.add("maddipineni");
+	//	System.out.println("");
+	Iterator<String> it=l.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
+			
+		}
+		
+		
+		
+		
+	}
+
+	@Override
+	public ListIterator listIterator(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void addFirst(Object arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addLast(Object arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Iterator descendingIterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object element() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean offer(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean offerFirst(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean offerLast(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object peekFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object peekLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object poll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object pollFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object pollLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void push(Object arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object remove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object removeFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean removeFirstOccurrence(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object removeLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean removeLastOccurrence(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
